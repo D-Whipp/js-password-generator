9b13a4d95f2303daf95c8a0466c6f5f75e5ba109
Mr Whipp <dwhipp88@gmail.com>
2021-07-04 13:35:44 -0600

    created my random generators

--- script.js
+++ script.js
@@ -1,5 +1,122 @@
 // Assignment code here
+// I need some random generators for lowercase, uppercase, numeric, and special character values
+// first let's focuse on lowercase
+var getRanLowercase = function () {
+  // need an array to hold the values I'm looking for
+  lowercaseArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
+  // now I need a way to grab one of these letters randomly
+  return lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)];
+};
+
+// Cool, now I need to do the same thing for the other characters
+// Random uppercase generator
+var getRanUppercase = function () {
+  uppercaseArr = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  return uppercaseArr[Math.floor(Math.random() * uppercaseArr.length)];
+};
+
+// Random number generator
+var getRanNumber = function () {
+  numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  return numbersArr[Math.floor(Math.random() * numbersArr.length)];
+};
+
+// Random symbol generator
+var getRanSymbol = function () {
+  symbolArr = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+
+  return symbolArr[Math.floor(Math.random() * symbolArr.length)];
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +127,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
